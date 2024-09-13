57cf75b691c38b106cef8d9b4aae0a5ef3d7fe12
kritesh2nd <kritesh2nd@gmail.com>
2024-09-13 13:41:06 +0545

    [1]starting merch web store project

---
+++ src/main/java/com/example/MerchWebStore/MerchWebStoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.MerchWebStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchWebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchWebStoreApplication.class, args);
+		System.out.println("Merch Web Store Application");
+	}
+
+}
---
+++ src/test/java/com/example/MerchWebStore/MerchWebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MerchWebStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchWebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
